e79d28139d7918d11cd49b79d348ce6b1f79d02c
wuhsh <haosheng.wu@polito>
2024-06-21 23:51:10 +0200

    add the main source code

---
+++ src/w3.cpp
@@ -0,0 +1,16 @@
+/* This is the main program of W3, which is a small project 
+to crete the 3D emc3 mesh. The main aims are:
+1. Create 1D plasma mesh, which can be used by ReMkit1D.
+2. Create 2D plasma mesh, which can be used by SOLPS-ITER. 
+3. Create 3D mesh, which can be used by EMC3-EIRENE.
+Contact: haosheng.wu@polito.it
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      cout << "Welcome to W3" << endl;
+      return 0;
+}
